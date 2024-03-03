306633cb1299ae81e890f056cfdf40fdec6df858
dhruvja <dhruv@iamsizzling.com>
2024-03-03 16:21:50 -0300

    initial commit for smart contracts

---
+++ programs/vaults/src/lib.rs
@@ -0,0 +1,304 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+//! # A Concordium V1 smart contract
+extern crate alloc;
+use alloc::collections::BTreeMap;
+use concordium_cis2::*;
+use concordium_std::*;
+use core::fmt::Debug;
+use mint_tokens::{BurnParams, MintParams};
+
+type Cis2ClientResult<T> = Result<T, Cis2ClientError<()>>;
+
+/// Your smart contract state.
+#[derive(Serialize, SchemaType, Debug, Clone)]
+pub struct State {
+    pub balances: BTreeMap<AccountAddress, u64>,
+}
+
+#[derive(Serialize, SchemaType)]
+pub struct DepositParams {
+    pub amount: u64,
+    pub receiver: Receiver,
+    pub contract_address: ContractIndex,
+    pub token_id: TokenIdU8,
+}
+
+/// The parameter type for the contract function `upgrade`.
+/// Takes the new module and optionally a migration function to call in the new
+/// module after the upgrade.
+#[derive(Serialize, SchemaType)]
+struct UpgradeParams {
+    /// The new module reference.
+    module: ModuleReference,
+    /// Optional entrypoint to call in the new module after upgrade.
+    migrate: Option<(OwnedEntrypointName, OwnedParameter)>,
+}
+
+/// Your smart contract errors.
+#[derive(Debug, PartialEq, Eq, Reject, Serialize, SchemaType)]
+pub enum Error {
+    /// Failed parsing the parameter.
+    #[from(ParseError)]
+    ParseParams, // 1
+    ParseResult,     // 2
+    InvalidResponse, // 3
+    /// Amount that was to be transferred is not available to the sender.
+    AmountTooLarge, // 4
+    /// Owner account of the smart contract that is being invoked does not
+    /// exist. This variant should in principle not happen, but is here for
+    /// completeness.
+    MissingAccount, // 5
+    /// Contract that is to be invoked does not exist.
+    MissingContract, // 6
+    /// The contract to be invoked exists, but the entrypoint that was named
+    /// does not.
+    MissingEntrypoint, // 7
+    /// Sending a message to the V0 contract failed.
+    MessageFailed, // 8
+    /// Contract that was called rejected with the given reason.
+    LogicReject {
+        // 9
+        reason: i32,
+        return_value: Cis2Error<()>,
+    },
+    /// Execution of a contract call triggered a runtime error.
+    Trap, // 10
+    /// Balance not found in the state
+    BalanceNotFound, // 11
+    /// When subtraction underflows
+    SubtractionUnderflow, // 12
+    /// When addtion overflows
+    AdditionOverflow, // 13
+}
+
+/// Init function that creates a new smart contract.
+#[init(contract = "vaults")]
+fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State> {
+    // Your code
+    // call LP token contract and initialize it
+    // store EuroE contract address
+
+    Ok(State {
+        balances: BTreeMap::new(),
+    })
+}
+
+impl From<Cis2ClientError<()>> for Error {
+    fn from(value: Cis2ClientError<()>) -> Self {
+        match value {
+            Cis2ClientError::InvokeContractError(e) => match e {
+                CallContractError::AmountTooLarge => Self::AmountTooLarge,
+                CallContractError::MissingAccount => Self::MissingAccount,
+                CallContractError::MissingContract => Self::MissingContract,
+                CallContractError::MissingEntrypoint => Self::MissingEntrypoint,
+                CallContractError::MessageFailed => Self::MessageFailed,
+                CallContractError::LogicReject {
+                    reason,
+                    return_value,
+                } => Self::LogicReject {
+                    reason,
+                    return_value,
+                },
+                CallContractError::Trap => Self::Trap,
+            },
+            Cis2ClientError::ParseResult => Self::ParseResult,
+            Cis2ClientError::InvalidResponse => Self::InvalidResponse,
+        }
+    }
+}
+
+#[receive(
+    contract = "vaults",
+    name = "deposit",
+    parameter = "DepositParams",
+    mutable,
+    enable_logger
+)]
+fn deposit(
+    ctx: &ReceiveContext,
+    host: &mut Host<State>,
+    _logger: &mut impl HasLogger,
+) -> ReceiveResult<()> {
+    let params: DepositParams = ctx.parameter_cursor().get()?;
+    let cis2_contract_address = ContractAddress::new(params.contract_address, 0);
+    let cis2_client = Cis2Client::new(cis2_contract_address);
+    ensure!(ctx.sender().is_account());
+    let sender = match ctx.sender() {
+        Address::Account(acc) => acc,
+        Address::Contract(_) => bail!(),
+    };
+    // Transfer tokens to the contract
+    let res: Cis2ClientResult<bool> = cis2_client.transfer(
+        host,
+        Transfer {
+            amount: TokenAmountU64(params.amount),
+            from: Address::Account(sender),
+            to: Receiver::Contract(
+                ctx.self_address(),
+                OwnedEntrypointName::new(String::from("depositIntoContract")).unwrap(),
+            ),
+            token_id: TokenIdU8(01),
+            data: AdditionalData::empty(),
+        },
+    );
+    if let Err(val) = res {
+        // logger.log_raw("Errored out here".as_bytes())?;
+        concordium_dbg!("This is the error {:?}", val);
+        bail!(Error::from(val).into());
+    }
+    // Mint LP tokens
+    let mint_params = MintParams {
+        to: ctx.sender(),
+        metadata_url: MetadataUrl {
+            url: "".to_string(),
+            hash: None,
+        },
+        token_id: TokenIdU8(01),
+        amount: concordium_cis2::TokenAmountU64(params.amount),
+    };
+    let res = host.invoke_contract(
+        &ContractAddress::new(2, 0),
+        &mint_params,
+        EntrypointName::new("mint").unwrap(),
+        Amount::zero(),
+    );
+    let _res = match res {
+        Ok(val) => {
+            let o = match val.1 {
+                Some(res) => Some(res),
+                None => None,
+            };
+            (val.0, o)
+        }
+        Err(err) => {
+            return Err(Error::from(
+                Cis2ClientError::<()>::try_from(err).map_err(|err| Error::from(err))?,
+            )
+            .into())
+        }
+    };
+    Ok(())
+}
+
+#[receive(contract = "vaults", name = "depositIntoContract", mutable)]
+fn deposit_into_contract(ctx: &ReceiveContext, host: &mut Host<State>) -> ReceiveResult<()> {
+    let params: OnReceivingCis2Params<TokenIdU8, TokenAmountU64> = ctx.parameter_cursor().get()?;
+    let (state, _builder) = host.state_and_builder();
+
+    ensure!(params.from.is_account());
+    let sender = match params.from {
+        Address::Account(acc) => acc,
+        Address::Contract(_acc) => bail!(),
+    };
+    let mut amount = params.amount.0;
+    let existing_balance = state.balances.get(&sender);
+    if let Some(balance) = existing_balance {
+        amount = amount
+            .checked_add(*balance)
+            .ok_or(Error::AdditionOverflow)?;
+    }
+    state.balances.insert(sender, amount);
+
+    Ok(())
+}
+
+#[receive(contract = "vaults", name = "withdraw", mutable)]
+fn withdraw(ctx: &ReceiveContext, host: &mut Host<State>) -> ReceiveResult<()> {
+    ensure!(ctx.sender().is_account());
+    let sender = match ctx.sender() {
+        Address::Account(acc) => acc,
+        Address::Contract(_) => bail!(),
+    };
+    let params: DepositParams = ctx.parameter_cursor().get()?;
+    let cis2_contract_address = ContractAddress::new(params.contract_address, 0);
+    let cis2_client = Cis2Client::new(cis2_contract_address);
+
+    // Transfer tokens from the contract
+    let res: Cis2ClientResult<bool> = cis2_client.transfer(
+        host,
+        Transfer {
+            amount: TokenAmountU64(params.amount),
+            from: Address::Contract(ctx.self_address()),
+            to: Receiver::Account(sender),
+            token_id: TokenIdU8(01),
+            data: AdditionalData::empty(),
+        },
+    );
+    if let Err(val) = res {
+        // logger.log_raw("Errored out here".as_bytes())?;
+        concordium_dbg!("This is the error {:?}", val);
+        bail!(Error::from(val).into());
+    }
+    // burn LP tokens
+    let mint_params = BurnParams {
+        owner: ctx.sender(),
+        token_id: TokenIdU8(01),
+        amount: concordium_cis2::TokenAmountU64(params.amount),
+    };
+    let res = host.invoke_contract(
+        &ContractAddress::new(2, 0),
+        &mint_params,
+        EntrypointName::new("burn").unwrap(),
+        Amount::zero(),
+    );
+    let _res = match res {
+        Ok(val) => {
+            let o = match val.1 {
+                Some(res) => Some(res),
+                None => None,
+            };
+            (val.0, o)
+        }
+        Err(err) => {
+            return Err(Error::from(
+                Cis2ClientError::<()>::try_from(err).map_err(|err| Error::from(err))?,
+            )
+            .into())
+        }
+    };
+
+    let user_balance = host
+        .state
+        .balances
+        .get_mut(&sender)
+        .ok_or(Error::BalanceNotFound)?;
+
+    *user_balance = user_balance
+        .checked_sub(u64::from(params.amount))
+        .ok_or(Error::SubtractionUnderflow)?;
+
+    Ok(())
+}
+
+#[receive(contract = "vaults", name = "view")]
+fn view(_ctx: &ReceiveContext, host: &Host<State>) -> ReceiveResult<(State, Amount)> {
+    let current_state = host.state();
+    let current_balance = host.self_balance();
+    Ok((current_state.clone(), current_balance))
+}
+
+#[receive(
+    contract = "vaults",
+    name = "upgrade",
+    parameter = "UpgradeParams",
+    low_level
+)]
+fn contract_upgrade(ctx: &ReceiveContext, host: &mut LowLevelHost) -> ReceiveResult<()> {
+    // Check that only the owner is authorized to upgrade the smart contract.
+    ensure!(ctx.sender().matches_account(&ctx.owner()));
+    // Parse the parameter.
+    let params: UpgradeParams = ctx.parameter_cursor().get()?;
+    // Trigger the upgrade.
+    host.upgrade(params.module)?;
+    // Call the migration function if provided.
+    if let Some((func, parameters)) = params.migrate {
+        host.invoke_contract_raw(
+            &ctx.self_address(),
+            parameters.as_parameter(),
+            func.as_entrypoint_name(),
+            Amount::zero(),
+        )?;
+    }
+    Ok(())
+}
---
+++ programs/vaults/tests/tests.rs
@@ -0,0 +1,287 @@
+use concordium_cis2::{TokenAmountU16, TokenIdU8};
+use concordium_smart_contract_testing::*;
+use concordium_std::MetadataUrl;
+use mint_tokens::*;
+use vaults::{DepositParams, Error, State};
+// use vaults::*;
+
+const ACC_ADDR_OWNER: AccountAddress = AccountAddress([0u8; 32]);
+const ACC_ADDR_OTHER: AccountAddress = AccountAddress([1u8; 32]);
+const ACC_INITIAL_BALANCE: Amount = Amount::from_ccd(1000);
+
+const DEPOSIT_AMOUNT: u64 = 1000;
+const WITHDRAW_AMOUNT: u64 = 500;
+
+fn setup_chain_and_contract() -> (
+    Chain,
+    ContractInitSuccess,
+    ContractInitSuccess,
+    ContractInitSuccess,
+) {
+    let mut chain = Chain::new();
+    let account_owner = Account::new(ACC_ADDR_OWNER, ACC_INITIAL_BALANCE);
+    let account_other = Account::new(ACC_ADDR_OTHER, ACC_INITIAL_BALANCE);
+    chain.create_account(account_owner);
+    chain.create_account(account_other);
+    let vaults_module = module_load_v1("dist/module.wasm.v1").expect("Module is valid and exists");
+    let token_module =
+        module_load_v1("../token/dist/module.wasm.v1").expect("Module is valid and exists");
+    let vaults_deployment = chain
+        .module_deploy_v1(Signer::with_one_key(), ACC_ADDR_OWNER, vaults_module)
+        .expect("Deploying valid module should succeed");
+    let token_deployment = chain
+        .module_deploy_v1(Signer::with_one_key(), ACC_ADDR_OWNER, token_module)
+        .expect("Deploying valid module should succeed");
+    let vaults_initialization = chain
+        .contract_init(
+            Signer::with_one_key(),
+            ACC_ADDR_OWNER,
+            Energy::from(10000),
+            InitContractPayload {
+                mod_ref: vaults_deployment.module_reference,
+                init_name: OwnedContractName::new_unchecked("init_vaults".to_string()),
+                param: OwnedParameter::empty(),
+                amount: Amount::zero(),
+            },
+        )
+        .expect("Initialization should always succeed");
+    let token_mint = TokenAmountU16(10000);
+    let euro_token_initialization = chain
+        .contract_init(
+            Signer::with_one_key(),
+            ACC_ADDR_OWNER,
+            Energy::from(10000),
+            InitContractPayload {
+                mod_ref: token_deployment.module_reference,
+                init_name: OwnedContractName::new_unchecked("init_cis2_multi".to_string()),
+                param: OwnedParameter::from_serial(&token_mint).unwrap(),
+                amount: Amount::zero(),
+            },
+        )
+        .expect("Initialization should always succeed");
+    let lp_token_initialization = chain
+        .contract_init(
+            Signer::with_one_key(),
+            ACC_ADDR_OWNER,
+            Energy::from(10000),
+            InitContractPayload {
+                mod_ref: token_deployment.module_reference,
+                init_name: OwnedContractName::new_unchecked("init_cis2_multi".to_string()),
+                param: OwnedParameter::from_serial(&token_mint).unwrap(),
+                amount: Amount::zero(),
+            },
+        )
+        .expect("Initialization should always succeed");
+    (
+        chain,
+        vaults_initialization,
+        euro_token_initialization,
+        lp_token_initialization,
+    )
+}
+
+#[test]
+fn test_init() {
+    let (mut chain, vaults_init, euro_token_init, lp_token_init) = setup_chain_and_contract();
+    let mint_params = MintParams {
+        to: Address::Account(ACC_ADDR_OWNER),
+        metadata_url: MetadataUrl {
+            url: String::from("Test"),
+            hash: None,
+        },
+        token_id: TokenIdU8(1),
+        amount: concordium_cis2::TokenAmountU64(10000),
+    };
+    let update = chain.contract_update(
+        Signer::with_one_key(),
+        ACC_ADDR_OWNER,
+        Address::Account(ACC_ADDR_OWNER),
+        Energy::from(10000),
+        UpdateContractPayload {
+            amount: Amount::zero(),
+            address: euro_token_init.contract_address,
+            receive_name: OwnedReceiveName::new_unchecked("cis2_multi.mint".to_string()),
+            message: OwnedParameter::from_serial(&mint_params).unwrap(),
+        },
+    );
+    assert!(update.is_ok(), "Minting tokens failed");
+    let tokens = chain
+        .contract_invoke(
+            ACC_ADDR_OTHER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: euro_token_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("cis2_multi.view".to_string()),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .unwrap();
+    let token_state: ViewState = from_bytes(&tokens.return_value).unwrap();
+    println!("These are tokens {:?}", token_state.state);
+
+    let deposit_params = DepositParams {
+        amount: DEPOSIT_AMOUNT,
+        receiver: concordium_cis2::Receiver::Contract(
+            vaults_init.contract_address,
+            OwnedEntrypointName::new("depositIntoContract".to_string()).unwrap(),
+        ),
+        contract_address: euro_token_init.contract_address.index,
+        token_id: TokenIdU8(1),
+    };
+    let update = chain
+        .contract_update(
+            Signer::with_one_key(),
+            ACC_ADDR_OWNER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(100000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: vaults_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("vaults.deposit".to_string()),
+                message: OwnedParameter::from_serial(&deposit_params).unwrap(),
+            },
+        )
+        .print_emitted_events();
+
+    if let Err(e) = update {
+        e.trace_elements.iter().for_each(|trace_element| {
+            match trace_element {
+                DebugTraceElement::WithFailures { contract_address: _, entrypoint: _, error, trace_elements: _, energy_used: _, debug_trace: _ } => {
+                    println!("error: {:?}", error);
+                    let return_value : Error = match error {
+                        InvokeExecutionError::Reject { reason: _, return_value } => from_bytes(&return_value).unwrap(),
+                        InvokeExecutionError::Trap { error } => panic!("error: {:?}", error),
+                    };
+                    println!("This is return value {:?}", return_value);
+                },
+                DebugTraceElement::Debug { entrypoint, address, debug_trace } => println!("This is entrypoint {:?}, address {:?} and debug_trace {:?}", entrypoint, address, debug_trace),
+                DebugTraceElement::Regular { entrypoint, trace_element, energy_used, debug_trace } => println!("Thsi is entrypoint {:?} trace_element: {:?}, energy_used {:?} debug_trace {:?}", entrypoint, trace_element, energy_used, debug_trace),
+            };
+        });
+    }
+    let vaults_view = chain
+        .contract_invoke(
+            ACC_ADDR_OTHER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: vaults_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("vaults.view".to_string()),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .unwrap();
+    let vaults_state: State = from_bytes(&vaults_view.return_value).unwrap();
+    let user_deposit_balance = vaults_state
+        .balances
+        .get(&ACC_ADDR_OWNER)
+        .expect("No deposit found with given address");
+
+    let lp_token_state = chain
+        .contract_invoke(
+            ACC_ADDR_OTHER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: lp_token_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("cis2_multi.view".to_string()),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .unwrap();
+    let lp_token_state: mint_tokens::ViewState = from_bytes(&lp_token_state.return_value).unwrap();
+    let lp_token_balance = lp_token_state.state[0].1.balances[0].1;
+    println!("This is lp token state {:?}", lp_token_state);
+
+    assert_eq!(
+        lp_token_balance,
+        concordium_cis2::TokenAmountU64(DEPOSIT_AMOUNT)
+    );
+    assert_eq!(user_deposit_balance, &DEPOSIT_AMOUNT);
+
+    let withdraw_params = DepositParams {
+        amount: WITHDRAW_AMOUNT,
+        receiver: concordium_cis2::Receiver::Contract(
+            vaults_init.contract_address,
+            OwnedEntrypointName::new("depositIntoContract".to_string()).unwrap(),
+        ),
+        contract_address: euro_token_init.contract_address.index,
+        token_id: TokenIdU8(1),
+    };
+    let update = chain
+        .contract_update(
+            Signer::with_one_key(),
+            ACC_ADDR_OWNER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(100000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: vaults_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("vaults.withdraw".to_string()),
+                message: OwnedParameter::from_serial(&withdraw_params).unwrap(),
+            },
+        )
+        .print_emitted_events();
+
+    if let Err(ref e) = update {
+        e.trace_elements.iter().for_each(|trace_element| {
+            match trace_element {
+                DebugTraceElement::WithFailures { contract_address: _, entrypoint: _, error, trace_elements: _, energy_used: _, debug_trace: _ } => {
+                    println!("error: {:?}", error);
+                    let return_value : Error = match error {
+                        InvokeExecutionError::Reject { reason: _, return_value } => from_bytes(&return_value).unwrap(),
+                        InvokeExecutionError::Trap { error } => panic!("error: {:?}", error),
+                    };
+                    println!("This is return value {:?}", return_value);
+                },
+                DebugTraceElement::Debug { entrypoint, address, debug_trace } => println!("This is entrypoint {:?}, address {:?} and debug_trace {:?}", entrypoint, address, debug_trace),
+                DebugTraceElement::Regular { entrypoint, trace_element, energy_used, debug_trace } => println!("Thsi is entrypoint {:?} trace_element: {:?}, energy_used {:?} debug_trace {:?}", entrypoint, trace_element, energy_used, debug_trace),
+            };
+        });
+    }
+
+    let vaults_view = chain
+        .contract_invoke(
+            ACC_ADDR_OTHER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: vaults_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("vaults.view".to_string()),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .unwrap();
+    let vaults_state: State = from_bytes(&vaults_view.return_value).unwrap();
+    let user_deposit_balance = vaults_state
+        .balances
+        .get(&ACC_ADDR_OWNER)
+        .expect("No deposit found with given address");
+
+    let lp_token_state = chain
+        .contract_invoke(
+            ACC_ADDR_OTHER,
+            Address::Account(ACC_ADDR_OWNER),
+            Energy::from(10000),
+            UpdateContractPayload {
+                amount: Amount::zero(),
+                address: lp_token_init.contract_address,
+                receive_name: OwnedReceiveName::new_unchecked("cis2_multi.view".to_string()),
+                message: OwnedParameter::empty(),
+            },
+        )
+        .unwrap();
+    let lp_token_state: mint_tokens::ViewState = from_bytes(&lp_token_state.return_value).unwrap();
+    let lp_token_balance = lp_token_state.state[0].1.balances[0].1;
+
+    assert_eq!(
+        lp_token_balance,
+        concordium_cis2::TokenAmountU64(DEPOSIT_AMOUNT - WITHDRAW_AMOUNT)
+    );
+    assert_eq!(user_deposit_balance, &(DEPOSIT_AMOUNT - WITHDRAW_AMOUNT));
+}
